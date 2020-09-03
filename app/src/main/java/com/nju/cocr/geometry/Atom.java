b530021cf287aece3693a2c707af3984082ec2b3
Xuguodong1999 <bugxu@outlook.com>
2020-09-03 14:47:19 +0800

    delete geometry

--- app/src/main/java/com/nju/cocr/geometry/Atom.java
+++
@@ -1,6 +0,0 @@
-package com.nju.cocr.geometry;
-
-import android.graphics.PointF;
-
-public class Atom extends Item<PointF> {
-}
--- app/src/main/java/com/nju/cocr/geometry/Bond.java
+++
@@ -1,6 +0,0 @@
-package com.nju.cocr.geometry;
-
-import android.graphics.PointF;
-
-public class Bond extends Item<PointF> {
-}
